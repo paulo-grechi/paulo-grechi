f3b08e7d96963edc16caaaf6f8b6d6aa77a6b09d
paulo-grechi <70842539+paulo-grechi@users.noreply.github.com>
2022-04-26 23:07:13 -0300

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -10,28 +10,15 @@ class MyClient(discord.Client):
 
         if message.content.startswith("!fuck"):
             await message.channel.send("haha NO")
-
-        if message.content.startswith("!death"):
-            await message.channel.send("`I support`")
-
+            
         if message.content.startswith("!life"):
             await message.channel.send("you're joking right? I mean life is trash I don't see the point.")
 
         if message.content.startswith("!lie"):
             await message.channel.send("HMM yes, life's only true certainty, aside from death that is...")
 
-    async def on_message_delete(message):
-        await message.channel.send("don't hide shit from me")
-
     async def on_ready(self):
-        print("am done you asshole")
-
-    async def on_typing(self, channel, user, when):
-        await channel.send("don't type you idiot")
-
-#    async def on_reaction_add(self, reaction, user):
-#        message = reaction.message
-#        await message.channel.send(user.mention + "just, don't")
+        print("loaded")
 
 
 client = MyClient()
